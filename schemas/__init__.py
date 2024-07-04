080d2f312d7b9dd279c4ecc0087c33c1d2f04cc8
marcosviana91 <marcos.viana.91@gmail.com>
2024-07-03 21:40:47 -0300

    return schemas

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,7 +1,7 @@
 # Used in TinyDB and JSON schemas
 
 
-class UserSchema:
+class User:
     '''
     Dados de identificação do usuário para login
     '''
@@ -23,3 +23,95 @@ class UserSchema:
         self.last_login = last_login
         self.real_name = real_name
         self.email = email
+
+# Salvo em Tiny DB
+
+
+class Player:
+    '''
+    Dados do usuário, relativos ao jogo (como jogador)
+    '''
+
+    def __init__(
+        self,
+        id: int,
+        xp_points: int = 0,
+        available_cards: list[str] = []
+    ):
+        self.id = id
+        self.xp_points = xp_points
+        self.available_cards = [
+            'abraao', 'adao', 'daniel',
+            'davi', 'elias', 'ester',
+            'eva', 'jaco', "jose-do-egito",
+            "josue", "maria", "moises",
+            "noe", "salomao", "sansao"
+        ] if available_cards == [] else available_cards
+
+    def onJoinMatch(self):
+        ...
+
+    def onEndMatch(self):
+        ...
+
+class APIResponseProps:
+    def __init__(
+        self,
+        message: str | None,
+    ):
+        self.message = message
+        self.data_type = 'error'
+        self.user_data: dict = {}
+        self.room_data: dict = {}
+        self.room_list: list = []
+        self.player_data: dict = {}
+        # self.player_in_match_data: Players_in_Match = None
+        # self.card_data: Card = None
+        # self.game_data: GameData = None
+
+    # @property
+    # def __dict__(self):
+    #     __temp = {
+    #         "data_type": self.data_type,
+    #         "user_data": self.user_data,
+    #         "room_data": self.room_data,
+    #         "player_data": self.player_data,
+    #     }
+    #     __reponse = {}
+
+    #     for k, v in __temp.items():
+    #         if v:
+    #             __reponse[k] = v
+
+    #     return __reponse
+
+
+class ClientRequestProps:
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        # print(__file__,'\nClientRequestProps.__init__\n' ,kwargs)
+        self.data_type: str = kwargs.get('data_type')
+        self.user_data: dict = kwargs.get("user_data")  # User
+        self.room_data: dict = kwargs.get("room_data")
+        self.player_data: dict = kwargs.get("player_data")
+        # self.player_in_match_data: dict = kwargs.get("player_in_match_data")
+        # self.card_data: dict = kwargs.get("card_data")
+        # self.game_data: dict = kwargs.get("game_data")
+
+    @property
+    def __dict__(self):
+        __temp = {
+            "data_type": self.data_type,
+            "user_data": self.user_data,
+            "room_data": self.room_data,
+            "player_data": self.player_data,
+        }
+        __reponse = {}
+
+        for k, v in __temp.items():
+            if v:
+                __reponse[k] = v
+
+        return __reponse
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,1 @@
-from .ConnectionManager import WS
 from .DataBaseManager import DB
-from .GameManager import GameRoom
-from .RoomManager import ROOMS
