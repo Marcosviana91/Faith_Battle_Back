d769ca0a158a689b718dce8d00b73b98878ef770
marcosviana91 <marcos.viana.91@gmail.com>
2024-07-03 23:48:57 -0300

    remove auth

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from secrets import token_hex
 
-from utils import DB
-from utils.populates import UserPopulate
+# from utils import DB
+# from utils.populates import UserPopulate
 
 import json
 
@@ -36,27 +36,27 @@ async def handleRoot(req: Request):
                            status_code=status.HTTP_200_OK)
     return res
 
-@app.post('/auth')
-async def handleAuth(req: Request):
-    data: dict = json.loads(await req.body())
-    user_data = DB.authUser(
-        username=data.get("username"),
-        password=data.get("password"),
-    )
-    if user_data.data_type == 'error':
-        res = JSONResponse(content=user_data.__dict__,
-                           status_code=status.HTTP_401_UNAUTHORIZED)
-    else:
-        req.session['user_info'] = user_data.user_data
-        print("session: ", req.session)
-        res = JSONResponse(content=user_data.__dict__,
-                           status_code=status.HTTP_202_ACCEPTED)
-    return res
+# @app.post('/auth')
+# async def handleAuth(req: Request):
+#     data: dict = json.loads(await req.body())
+#     user_data = DB.authUser(
+#         username=data.get("username"),
+#         password=data.get("password"),
+#     )
+#     if user_data.data_type == 'error':
+#         res = JSONResponse(content=user_data.__dict__,
+#                            status_code=status.HTTP_401_UNAUTHORIZED)
+#     else:
+#         req.session['user_info'] = user_data.user_data
+#         print("session: ", req.session)
+#         res = JSONResponse(content=user_data.__dict__,
+#                            status_code=status.HTTP_202_ACCEPTED)
+#     return res
 
 
-@app.post('/newuser')
-async def handleNewUser(req: Request):
-    data = json.loads(await req.body())
-    user_data = DB.createNewUser(data)
-    res = JSONResponse(content=user_data.__dict__, status_code=status.HTTP_201_CREATED)
-    return res
+# @app.post('/newuser')
+# async def handleNewUser(req: Request):
+#     data = json.loads(await req.body())
+#     user_data = DB.createNewUser(data)
+#     res = JSONResponse(content=user_data.__dict__, status_code=status.HTTP_201_CREATED)
+#     return res
