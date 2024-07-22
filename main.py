861411acad1a8afb9d92f70c1c8d6650b0e54818
marcosviana91 <marcos.viana.91@gmail.com>
2024-07-22 16:48:21 -0300

    conflitos

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-from secrets import token_hex
-
-<<<<<<< HEAD
-# from utils import DB
-# from utils.populates import UserPopulate
-
-import json
-=======
-=======
->>>>>>> room_websocket
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
->>>>>>> only_auth
 
 from routers import auth, room, users, websocket
 from settings import env_settings
@@ -41,38 +29,6 @@ app.add_middleware(
 )
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-<<<<<<< HEAD
-@app.get('/')
-async def handleRoot(req: Request):
-    res = JSONResponse(content={"message": "Root router ok"},
-                           status_code=status.HTTP_200_OK)
-    return res
-
-# @app.post('/auth')
-# async def handleAuth(req: Request):
-#     data: dict = json.loads(await req.body())
-#     user_data = DB.authUser(
-#         username=data.get("username"),
-#         password=data.get("password"),
-#     )
-#     if user_data.data_type == 'error':
-#         res = JSONResponse(content=user_data.__dict__,
-#                            status_code=status.HTTP_401_UNAUTHORIZED)
-#     else:
-#         req.session['user_info'] = user_data.user_data
-#         print("session: ", req.session)
-#         res = JSONResponse(content=user_data.__dict__,
-#                            status_code=status.HTTP_202_ACCEPTED)
-#     return res
-
-
-# @app.post('/newuser')
-# async def handleNewUser(req: Request):
-#     data = json.loads(await req.body())
-#     user_data = DB.createNewUser(data)
-#     res = JSONResponse(content=user_data.__dict__, status_code=status.HTTP_201_CREATED)
-#     return res
-=======
 @app.get("/")
 def handleRoot():
     res = {"message": "Root router ok"}
@@ -84,9 +40,6 @@ def handlePopulate():
     from utils.populates import UserPopulate
     res = {"message": "Populated"}
     return res
-<<<<<<< HEAD
->>>>>>> only_auth
-=======
 
 @app.get("/fake_match")
 async def makeFakeMatch():
@@ -100,4 +53,4 @@ async def makeFakeMatch():
 async def getFakeMatch():
     res = MATCHES.getStats
     return res
->>>>>>> room_websocket
+
